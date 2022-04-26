b67e8e1c74e208cb09e71fe25e32e101c49b9609
MariaDuda <mariabevenuto062@gmail.com>
2022-04-26 12:56:34 -0300

    rota dos participantes

--- back-end/app.js
+++ back-end/app.js
@@ -7,7 +7,22 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+const participantes =[];//{name: 'João', lastStatus: 12313123} // O conteúdo do lastStatus será explicado nos próximos requisitos
+const mensagens = [];//{from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}
 
+
+app.post("/participants" , (req , res) => {
+ const { name } = req.body
+if(name == ""){
+    res.status(422).send("Todos os campos são obrigatórios!");
+    return;
+}
+if(name === name){
+    es.status(409).send("Este nome já existe");
+    return;
+}
+participantes.push(name)
+})
 
 
 
