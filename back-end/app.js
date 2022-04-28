e4cd0fa3fe680710c36502e55575a739ccfecd5c
MariaDuda <mariabevenuto062@gmail.com>
2022-04-28 13:48:25 -0300

    ajuste baseado na aula de ontem

--- back-end/app.js
+++ back-end/app.js
@@ -7,6 +7,7 @@ import dayjs from 'dayjs';
 import { appendFile } from "fs";
 import joi from "joi";
 
+
 dotenv.config();
 
 const app = express();
@@ -17,8 +18,16 @@ app.use(express.json());
 const participantes=[];
 const messages = [];
 
-let db;
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+let db = null;
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+const promise = mongoClient.connect();
+
+promise.then(() =>{
+  db = mongoClient.db("api_bate_papo_uol");
+  console.log("conectou ao banco do bate-papo-uol");
+})
+promise.catch(res => console.log(chalk.red("deu xabu"), res))
+
 
 const participanteValidar= joi.object({
   name: joi.string().required()
@@ -30,34 +39,66 @@ const messageValidar = joi.object({
   type: joi.string().valid('message', 'private_message'),
 })
 
-app.post("/participants", (req, res) => {
-  /*  FIXME("VALIDAÇÃO COM A BLIBIOTECA JOI") */
-  const participante = req.body.name
 
-  const participanteArray = mongoClient.db("bate-papo-uol").collection("participants");
-  const nomeParticipanteTrue = participanteArray.findOne({ name: participante.name });
-
-  const validar = participanteValidar.validate(participante);
-  if (validar.error) {
+//era pra colocar as mensagens e os posts por isso o post de participants era tão grande ;-;
+app.post('/participants', async (req, res) => {
+  async function EnviarParticipante()
+  const participant = req.body;
+ 
+  const validation = participanteValidar.validate(participant);
+  if (validation.error) {
     return res.sendStatus(422)
   }
 
-  if (nomeParticipanteTrue) {
-    return res.sendStatus(409);
+  try {
+    const mongoClient = new MongoClient(process.env.MONGO_URL);
+    await mongoClient.connect()
+
+    const participanteArray = mongoClient.db("bate-papo-uol").collection("participants");
+    const messagesCollection = mongoClient.db("bate-papo-uol").collection("messages");
+
+    const participanteExiste = await participanteArray.findOne({ name: participant.name });
+    if (participanteExiste) {
+      return res.sendStatus(409);
+    }
+
+    await participanteArray.insertOne({ ...participant, lastStatus: Date.now() });
+
+    await messagesCollection.insertOne({
+      from: participant.name,
+      to: 'Todos',
+      text: 'entra na sala...',
+      type: 'status',
+      time: dayjs().format("HH:mm:ss")
+    });
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
   }
-   participanteArray.insertOne({ ...participante, lastStatus: Date.now() });
-   participantes.push(participanteArray)
-})
+  EnviarParticipante()
+});
+
+
 
 
 
 app.get("/participants", (req, res) => {
-  /* await */ mongoClient.connect()
-  res.send(participantes)
-})
+  async function participanteAchar() {
+    try {
+      let participants = await db.collection("participants").find().toArray();
+      res.status(200).send(participants);
+    } catch (error) {
+      res.send(404).send("desculpe, mas não conseguimos achar o participante" );
+    }
+  }
+  participanteAchar();
+});
+
 
 
 app.post("/messages", (req, res) => {
+  async function EnviarMessage() 
   const { to, text, type } = req.body;
   const from = req.headers.user;
 
@@ -65,6 +106,7 @@ app.post("/messages", (req, res) => {
   const ValidarText = messageValidar.validate(text) 
   const ValidarType = messageValidar.validate(type)
 
+  const participanteArray = mongoClient.db("bate-papo-uol").collection("participants");
   const messagesArray = mongoClient.db("bate-papo-uol").collection("messages");
 
   if (!ValidarTo) {
@@ -75,23 +117,21 @@ app.post("/messages", (req, res) => {
     res.status(422).send("Todos os campos são obrigatórios!");
     return;
   }
-  const nomeParticipanteTrue =  participantes.findOne({ name: from })
+  const nomeParticipanteTrue =  participanteArray.findOne({ name: from })
   if (!nomeParticipanteTrue) {
     return res.sendStatus(422);
   }
-  messagesArray.insertOne({
-    from: participantes.name,
-    to: to,
-    text: text,
-    type: type,
+
+  await db.messagesArray.insertOne({
+    from: participanteArray.name,
+    to: [...to],
+    text: [...text],
+    type: [...type],
     time:dayjs().format("HH:MM:SS")
-})
-messages.push(messagesArray)
-res.sendStatus(201);
-})
-
-
-
+  });
+  res.sendStatus(201);
+  EnviarMessage()
+});
 
 
 
